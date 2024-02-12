2de99bd8c1294107424b8d20519589e19adfcb97
Junzdev <109050772+Junzdev@users.noreply.github.com>
2024-02-12 09:09:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
+const dataPath = path.join(__dirname, 'data');
 
-app.get('/image/:id', (req, res) => {
- const id = req.params.id;
- const images = require(`./image/${id}.json`);
- const randomIndex = Math.floor(Math.random() * images.length);
- const randomImage = images[randomIndex];
- const result = { url: randomImage, author: 'Jun' };
- res.json(result);
+app.get('/', (req, res) => {
+  const { data } = req.query;
+  const filePath = path.join(dataPath, `${data}.json`);
+  
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({ error: 'Data not found' });
+  }
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const jsonData = JSON.parse(data);
+    const randomIndex = Math.floor(Math.random() * jsonData.length);
+    const randomData = jsonData[randomIndex];
+
+    const { Author, Url, Title } = randomData;
+    const response = { Author, Url, Title };
+
+    res.json(response);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
- console.log(`Server listening on port ${PORT}`);
-}); 
+  console.log(`Server listening on port ${PORT}`);
+});
